a77472a2204bab1907b8c26bc94587aafee09052
Galah Seno Adjie <galahsenoadjie@gmail.com>
2020-12-04 20:28:54 +0700

    sub3 pagination

--- app/src/main/java/com/example/moviescatalogue/data/remote/ApiServices.kt
+++ app/src/main/java/com/example/moviescatalogue/data/remote/ApiServices.kt
@@ -10,7 +10,7 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiServices {
-    @GET("discover/movie?api_key=${BuildConfig.API_TOKEN}&sort_by=popularity.desc&page=1")
+    @GET("discover/movie")
     suspend fun getListMovies(
         @Query("api_key") apiKey: String = BuildConfig.API_TOKEN,
         @Query("sort_by") sortBy: String = "popularity.desc",
